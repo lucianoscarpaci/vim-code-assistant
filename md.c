2db0c71896f93b4cbb5ff89a1dc6f8124e1b3b27
Luciano Scarpaci <tauxsik@gmail.com>
2023-10-01 15:49:51 -0400

    Update md.c

--- md.c
+++ md.c
@@ -1,8 +1,19 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 
 int main() {
-   system("python3 ~/Documents/Github/vim-code-assistant/md.py");
-   return 0; 
+    // Get the home directory from the environment variables
+    const char* homeDir = getenv("HOME");
+
+    // Create a command to touch a new .md file with the current timestamp
+    char create_md[200];
+    snprintf(create_md, sizeof(create_md), "touch \"%s/Library/Application Support/Sublime Text/Packages/User/Chpt/$(date +%%Y-%%m-%%d-%%I.%%M.%%S-%%p).md\"", homeDir);
+    system(create_md);
+
+    // Open the latest .md file using the subl command
+    char open_subl[200];
+    snprintf(open_subl, sizeof(open_subl), "cd \"%s/Library/Application Support/Sublime Text/Packages/User/Chpt/\" && subl \"$(ls -t *.md | head -1)\"", homeDir);
+    system(open_subl);
+
+    return 0;
 }
